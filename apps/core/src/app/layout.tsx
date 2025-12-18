bcc9e45d067b193c8003470f4c4e71dfd3b79e6b
John Kraczek <john.kraczek@gmail.com>
2025-12-18 00:29:36 -0700

    CSS WORKING

--- apps/core/src/app/layout.tsx
+++ apps/core/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Providers } from "~/context/providers";
 import { Toaster } from 'sonner'
-import '~/index.css'
+import '@ydtb/ui/globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
