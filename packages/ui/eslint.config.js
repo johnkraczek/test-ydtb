770237f6eb67eb4ad43d0d4677fbe9b3ae5455fe
John Kraczek <john.kraczek@gmail.com>
2025-12-18 00:09:46 -0700

    Add UI Package

---
+++ packages/ui/eslint.config.js
@@ -0,0 +1,4 @@
+import { config } from "@ydtb/eslint-config/react-internal"
+
+/** @type {import("eslint").Linter.Config} */
+export default config
---
+++ packages/ui/src/components/ui/index.ts
@@ -0,0 +1,84 @@
+// UI Components
+export { Alert, AlertDescription, AlertTitle } from "./alert"
+export { Avatar, AvatarFallback, AvatarImage } from "./avatar"
+export { Badge } from "./badge"
+export { Button, buttonVariants } from "./button"
+export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./card"
+export { Checkbox } from "./checkbox"
+export {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "./command"
+export {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+  DialogTrigger,
+} from "./dialog"
+export {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "./dropdown-menu"
+export { Input } from "./input"
+export { InputOTP, InputOTPGroup, InputOTPSeparator, InputOTPSlot } from "./input-otp"
+export { Label } from "./label"
+export { Popover, PopoverContent, PopoverTrigger } from "./popover"
+export { Progress } from "./progress"
+export { ScrollArea, ScrollBar } from "./scroll-area"
+export {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectScrollDownButton,
+  SelectScrollUpButton,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from "./select"
+export { Separator } from "./separator"
+export {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetOverlay,
+  SheetPortal,
+  SheetTitle,
+  SheetTrigger,
+} from "./sheet"
+export { Slider } from "./slider"
+export { Switch } from "./switch"
+export { Tabs, TabsContent, TabsList, TabsTrigger } from "./tabs"
+export { Textarea } from "./textarea"
+export { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./tooltip"
+export { VisuallyHidden } from "./visually-hidden"
